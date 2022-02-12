aee8ae16586b6ab89d9be56472164990308506a9
Nico <nrosa01@ucm.es>
2022-02-12 01:06:17 +0100

    Ahora funciona el drag básico en UI

--- Assets/Scripts/Question System/UIImageDrag.cs
+++ Assets/Scripts/Question System/UIImageDrag.cs
@@ -8,9 +8,11 @@ public class UIImageDrag : MonoBehaviour, IDragHandler, IPointerExitHandler
     [SerializeField] RectTransform container;
     RectTransform pos;
     Vector2 anchoredPos;
+    int UILayer;
 
     private void Awake()
     {
+        UILayer = LayerMask.NameToLayer("UI");
         pos = GetComponent<RectTransform>();
         anchoredPos = pos.anchoredPosition;
     }
@@ -20,20 +22,39 @@ public class UIImageDrag : MonoBehaviour, IDragHandler, IPointerExitHandler
         this.transform.position += (Vector3)eventData.delta;
     }
 
-    public void OnPointerUp(PointerEventData eventData)
-    {
-
-    }
-
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (container.rect.Contains(anchoredPos))
+        if (IsPointerOverUIElement())
         {
             pos.anchoredPosition = anchoredPos;
-            Debug.Log("Hola");
-
+        }
+        else
+        {
+            Destroy(this.gameObject);
         }
         ;
+    }
+
+    public bool IsPointerOverUIElement() => IsPointerOverUIElement(GetEventSystemRaycastResults());
+    
+    private bool IsPointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
+    {
+        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+        {
+            RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+            if (curRaysastResult.gameObject.layer == UILayer)
+                return true;
+        }
+        return false;
+    }
+
+    static List<RaycastResult> GetEventSystemRaycastResults()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raysastResults);
+        return raysastResults;
     }
 
     //private void OnMouseDrag()
